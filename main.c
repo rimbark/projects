e971a6a4cb6e68221c303ecd8bc4a71864b27084
rimbark <100283992+rimbark@users.noreply.github.com>
2022-02-23 19:27:37 +0700

    Update main.c
    decomposition

--- main.c
+++ main.c
@@ -3,84 +3,101 @@
 #include <time.h>
 
 void set_columns(int* b, int row);
-void sorting(int* b, int **a, int row);
+void initialization(int** a, int* b, int row);
+void sorting(int* b, int** a, int row);
+void output(int** a, int* b, int row);
+void mem_free(int** a, int* b, int row);
 
 int main() {
-		int** a;
-		int* b;
-		int row_of_arr, randomiser, num, i, j;
-		srand(time(NULL));
-		  printf("Insert num: ");
-		  scanf("%d", &row_of_arr);
-		a = (int**)malloc(row_of_arr * sizeof(int*));
-		b = (int *)malloc(row_of_arr * sizeof(int));
-		set_columns(b, row_of_arr);
-		for (i = 0; i < row_of_arr; i++) {
-				a[i] = (int*)malloc(b[i] * sizeof(int));
-				for (j = 0; j < b[i]; j++) {
-						randomiser = rand();
-						num = 1 + randomiser % 99;
-						a[i][j] = num;
-				}
-		}
-		sorting(b, a, row_of_arr);
-		for (i = 0; i < row_of_arr; i++) {
-				for (j = 0; j < b[i]; j++) {
-						printf("%5d", a[i][j]);
-				}
-				printf("\n");
-		}
-		for (i = 0; i < row_of_arr; i++)
-				free(a[i]);
-		free(a);
-		free(b);
-		return 0;
+  int** a;
+  int* b;
+  int size_of_arr;
+  srand(time(NULL));
+  printf("Insert num (from 1 to 20): ");
+  scanf("%d", &size_of_arr);
+  a = (int**)malloc(size_of_arr * sizeof(int*));
+  b = (int*)malloc(size_of_arr * sizeof(int));
+  set_columns(b, size_of_arr);
+  initialization(a, b, size_of_arr);
+  sorting(b, a, size_of_arr);
+  output(a, b, size_of_arr);
+  mem_free(a, b, size_of_arr);
+  return 0;
 }
 
 void set_columns(int* b, int row) {
-		int randomiser, col, j;
-		for (int i = 0; i < row; i++) {
-				randomiser = rand();
-				col = 1 + randomiser % 20;
-				b[i] = col;
-				if (i > 0) {
-						j = 0;
-						while (j < i) {
-								if (b[i] == b[j]) {
-										j = 0;
-										randomiser = rand();
-										col = 1 + randomiser % 20;
-										b[i] = col;
-								} else {
-										j++;
-								}
-						}
-				}
-		}
+  int randomiser, col, j;
+  for (int i = 0; i < row; i++) {
+    randomiser = rand();
+    col = 1 + randomiser % 21;
+    b[i] = col;
+    if (i > 0) {
+      j = 0;
+      while (j < i) {
+        if (b[i] == b[j]) {
+          j = 0;
+          randomiser = rand();
+          col = 1 + randomiser % 21;
+          b[i] = col;
+        } else {
+          j++;
+        }
+      }
+    }
+  }
 }
-void sorting(int* b, int **a, int row) {
-		int i, j, s, tmp;
-		for (i = 0; i < row; i++) {
-				if (i % 2 == 0) {
-						for (s = 0; s < b[i]; s++) {
-								for (j = b[i] - 1; j > s; j--) {
-										if (a[i][j] < a[i][j - 1]) {
-												tmp = a[i][j];
-												a[i][j] = a[i][j - 1];
-												a[i][j - 1] = tmp;
-										}
-								}
-						}
-				} else {
-						for (s = 0; s < b[i]; s++) {
-								for (j = b[i] - 1; j > s; j--) {
-										if (a[i][j] > a[i][j - 1]) {
-												tmp = a[i][j - 1];
-												a[i][j - 1] = a[i][j];
-												a[i][j] = tmp;
-										}
-								}
-						}
-				}
-		}
+
+void initialization(int** a, int* b, int row) {
+  int randomiser, num;
+  for (int i = 0; i < row; i++) {
+    a[i] = (int*)malloc(b[i] * sizeof(int));
+    for (int j = 0; j < b[i]; j++) {
+      randomiser = rand();
+      num = 1 + randomiser % 99;
+      a[i][j] = num;
+    }
+  }
+}
+
+void sorting(int* b, int** a, int row) {
+  int i, j, s, tmp;
+  for (i = 0; i < row; i++) {
+    if (i % 2 == 0) {
+      for (s = 0; s < b[i]; s++) {
+        for (j = b[i] - 1; j > s; j--) {
+          if (a[i][j] < a[i][j - 1]) {
+            tmp = a[i][j];
+            a[i][j] = a[i][j - 1];
+            a[i][j - 1] = tmp;
+          }
+        }
+      }
+    } else {
+      for (s = 0; s < b[i]; s++) {
+        for (j = b[i] - 1; j > s; j--) {
+          if (a[i][j] > a[i][j - 1]) {
+            tmp = a[i][j - 1];
+            a[i][j - 1] = a[i][j];
+            a[i][j] = tmp;
+          }
+        }
+      }
+    }
+  }
+}
+
+void output(int** a, int* b, int row) {
+  for (int i = 0; i < row; i++) {
+    printf("Arr size is - [%d]\n", b[i]);
+    for (int j = 0; j < b[i]; j++) {
+      printf("%5d", a[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void mem_free(int** a, int* b, int row) {
+  for (int i = 0; i < row; i++) free(a[i]);
+  free(a);
+  free(b);
 }
